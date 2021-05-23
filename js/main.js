b3982a6003e10b6c88f106b5effcd513d2dd8ca1
Yashmita <55138349+yashmita@users.noreply.github.com>
2021-05-23 14:29:44 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,14 +20,6 @@ $(document).ready(function(){
     }
   });
 
-
-  $('.accordion-header').click(function(){
-    $('.accordion .accordion-body').slideUp();
-    $(this).next('.accordion-body').slideDown();
-    $('.accordion .accordion-header span').text('+');
-    $(this).children('span').text('-');
-  });
-
 });
 
 // OVERLAY
